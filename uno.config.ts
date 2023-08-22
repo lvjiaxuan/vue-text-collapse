078d2ed0a42fde00fa6a3ad44dee6b36794be828
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-08-22 14:53:42 +0800

    fix: hack for Intellisense

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'unocss'
+import { type UserConfig, defineConfig } from 'unocss'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export default defineConfig({
   cli: {
     entry: {
@@ -9,4 +10,4 @@ export default defineConfig({
     },
   },
   transformers: [ transformerVariantGroup() ],
-})
+}) as UserConfig
